d26e98bb2f1769c802ae176100ac9260d094185a
Matrez21 <matias.araya05@alumnos.ucn.cl>
2024-12-11 21:09:23 -0300

    Comparation y PaymentMethod agregados

---
+++ src/components/AppPages/Comparation.jsx
@@ -0,0 +1,54 @@
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    Button,
+    TextField,
+    Tabs,
+    Tab,
+    Grid,
+    Card,
+    CardContent,
+    CardMedia,
+    Modal,
+    Box,
+    Container,
+  } from '@mui/material';
+  import IconButton from '@mui/material/IconButton';
+  import MenuIcon from '@mui/icons-material/Menu';
+  import '../AppPages/GeneralPage.css'
+  import { Link } from 'react-router-dom';
+
+
+const Comparation = ()=>{
+    return(
+        <div className='body'>
+        <Tabs
+            textColor="secondary"
+            indicatorColor="primary"
+            aria-label="secondary tabs example"
+            centered
+            sx={{backgroundColor:"#393838",
+                 '& .MuiTab-root': {
+                 minWidth: 'auto',
+                 paddingX: 4,
+                fontWeight: 500,
+                fontFamily: 'Arial, sans-serif',
+                '&:hover': { color: '#9b9b9b' },
+          },
+        }}
+      >
+        <Tab value="one" label="Resumen" component={Link} to="/summary" sx={{ color: "white" }} />
+        <Tab value="two" label="Finanzas" component={Link} to="/finance" sx={{ color: "white" }} />
+        <Tab value="three" label="Gráficos" component={Link} to="/graphics" sx={{ color: "white" }} />
+        <Tab value="four" label="Productos" component={Link} to="/inventory" sx={{ color: "white" }} />
+        <Tab value="five" label="Medios de pago" component={Link} to="/paymethod" sx={{ color: "white" }} />
+        <Tab value="six" label="Comparacion" component={Link} to="/comparation" sx={{ color: "white" }} />
+    </Tabs>      
+        <Typography>
+            Comparacion
+        </Typography>
+    </div>
+    );
+}
+export default Comparation;

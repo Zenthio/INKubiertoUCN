05e6a6cdaa179dc640eccbef8378df5f4fc4a822
Zehillua <ignaciovicente04@gmail.com>
2024-12-14 19:52:37 -0300

    Actualización final del back

--- src/paymethod/paymethod.service.ts
+++ src/paymethod/paymethod.service.ts
@@ -8,7 +8,7 @@ export class PayMethodService {
   async getUsageByPaymentMethod(): Promise<{ [key: string]: number }> {
     const currentYear = new Date().getFullYear();
     const currentMonth = new Date().getMonth(); // Meses en JavaScript van de 0 a 11
-    const firstDayOfMonth = new Date(Date.UTC(currentYear, currentMonth, 1, 0, 0, 0));
+    const firstDayOfMonth = new Date(Date.UTC(currentYear, currentMonth, 1, 0, 0, 0)); //puede tener error 
     const firstDayOfNextMonth = new Date(Date.UTC(currentYear, currentMonth + 1, 1, 0, 0, 0));
 
     const pagos = await this.prisma.pago.findMany({
